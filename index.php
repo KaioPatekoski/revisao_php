373861aedf36621b2c0d6dd860b7b0740819ed00
Kaio Patekoski <kaiopatekoski33@gmail.com>
2026-03-11 16:32:02 -0300

    if simples php

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
- echo("revisao");
+ echo "revisao";
+
+ $nome = "Ana";
+ $nota = 7;
+
+ if ($nota >= 6){
+    echo "aprovado";
+ }
+ else {
+    echo "reprovado";
+ }
+
+ for ($i = 1; $i <= 3; $i++){
+    echo "rodada $i";
+ }
